3fefe97451243c486953ac9cef844195ea0bd049
Peter Hutterer <peter.hutterer@who-t.net>
2015-08-13 11:35:25 +1000

    Scaffolding for a libratbag, a mouse configuration library
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

---
+++ test/build-cxx.cc
@@ -0,0 +1,11 @@
+#include <libratbag.h>
+
+/* This is a build-test only */
+
+using namespace std;
+
+int
+main(int argc, char **argv)
+{
+	return 0;
+}
---
+++ test/build-pedantic.c
@@ -0,0 +1,9 @@
+#include <libratbag.h>
+
+/* This is a build-test only */
+
+int
+main(void)
+{
+	return 0;
+}
