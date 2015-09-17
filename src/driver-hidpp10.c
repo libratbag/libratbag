249ad1e639ecac809fbd173571dac3a00e6028dc
Clément Vuchener <clement.vuchener@gmail.com>
2015-09-17 16:11:07 +0200

    hidpp10: add G5 2007

--- src/driver-hidpp10.c
+++ src/driver-hidpp10.c
@@ -285,6 +285,9 @@ static const struct ratbag_id hidpp10drv_table[] = {
 	/* G5 */
 	{ .id = LOGITECH_DEVICE(BUS_USB, 0xc041) },
 
+	/* G5 2007 */
+	{ .id = LOGITECH_DEVICE(BUS_USB, 0xc049) },
+
 	/* G9 */
 	{ .id = LOGITECH_DEVICE(BUS_USB, 0xc048) },
 
