fcbc930ca3cc061c3037a332764fe5fd4f0de275
Peter Hutterer <peter.hutterer@who-t.net>
2015-10-13 08:55:38 +1000

    Add the build script used by SemaphoreCI

---
+++ semaphore/semaphore-build-script.sh
@@ -0,0 +1,11 @@
+# This is the build script used for
+# https://semaphoreci.com/libratbag/libratbag/
+#
+# When updating the build settings, please update this script too.
+
+sudo apt-get update
+sudo apt-get install -y valgrind check libevdev-dev libudev-dev doxygen
+graphviz
+autoreconf -ivf
+./configure --prefix=$PWD/_build --with-udev-base-dir=$PWD/_build
+DISTCHECK_CONFIGURE_FLAGS="--with-udev-base-dir=$PWD/_build" make distcheck
