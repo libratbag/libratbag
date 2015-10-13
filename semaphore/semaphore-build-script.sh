9089478e2914a7a5bbc21eebd658ae7299fa49cc
Peter Hutterer <peter.hutterer@who-t.net>
2015-10-13 11:28:30 +1000

    semaphore: add the two lines we're now using as build script in semaphore
    Rather than copy/pasting the script, let semaphore fetch it and execute it.
    This way we only need to maintain it in one place.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- semaphore/semaphore-build-script.sh
+++ semaphore/semaphore-build-script.sh
@@ -1,7 +1,10 @@
 # This is the build script used for
 # https://semaphoreci.com/libratbag/libratbag/
 #
-# When updating the build settings, please update this script too.
+# semaphore uses this script directly, use the following two commands as
+# semaphore's build script:
+#  curl  https://raw.githubusercontent.com/libratbag/libratbag/master/semaphore/semaphore-build-script.sh > build-script.sh
+#  sh ./build-script.sh
 
 sudo apt-get update
 sudo apt-get install -y valgrind check libevdev-dev libudev-dev doxygen graphviz
