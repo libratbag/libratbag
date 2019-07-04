c1265bc98af90fb1cbb0ad02153b888c009d3c43
Filipe Laíns <lains@archlinux.org>
2019-07-04 11:33:42 +0100

    hidpp10: fix segfault for the c531 (and possibly other 27MHz receivers)
    hidpp10_read_memory will return the HID++ error if the request fails.
    right now, the error code from hidpp10_read_memory is transparently
    passed through hidpp10_onboard_profiles_read_macro. This is problematic
    because hidpp10_onboard_profiles_parse_macro only aborts on rc < 0 and
    HID++ error are > 0.
    
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- src/hidpp10.c
+++ src/hidpp10.c
@@ -1161,7 +1161,7 @@ hidpp10_onboard_profiles_parse_macro(struct hidpp10_device *device,
 	hidpp_log_raw(&device->base, "*** macro starts at (0x%02x, 0x%04x) ***\n", page, offset);
 
 	rc = hidpp10_onboard_profiles_read_macro(device, page, offset, &macro);
-	if (rc < 0)
+	if (rc)
 		return rc;
 
 	count = rc;
