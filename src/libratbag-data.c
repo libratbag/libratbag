196d53e65fc9c11925407e365ac541c0cb3cfaca
Filipe Laíns <lains@archlinux.org>
2019-07-31 08:28:15 +0100

    hidpp20: add quirk system
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- src/libratbag-data.c
+++ src/libratbag-data.c
@@ -33,6 +33,7 @@
 #include "libratbag.h"
 #include "libratbag-private.h"
 #include "libratbag-data.h"
+#include "hidpp20.h"
 
 #define GROUP_DEVICE "Device"
 
@@ -54,6 +55,7 @@ enum driver {
 
 struct data_hidpp20 {
 	int index;
+	unsigned quirk;
 };
 
 struct data_hidpp10 {
@@ -152,6 +154,7 @@ init_data_hidpp20(struct ratbag *ratbag,
 	const char *group = "Driver/hidpp20";
 	GError *error = NULL;
 	int num;
+	char *str;
 
 	data->hidpp20.index = -1;
 
@@ -160,6 +163,9 @@ init_data_hidpp20(struct ratbag *ratbag,
 		data->hidpp20.index = num;
 	if (error)
 		g_error_free(error);
+
+	str = g_key_file_get_string(keyfile, group, "Quirk", NULL);
+	data->hidpp20.quirk = HIDPP20_QUIRK_NONE;
 }
 
 static void
@@ -560,6 +566,14 @@ ratbag_device_data_hidpp20_get_index(const struct ratbag_device_data *data)
 	assert(data->drivertype == HIDPP20);
 
 	return data->hidpp20.index;
+}
+
+int
+ratbag_device_data_hidpp20_get_quirk(const struct ratbag_device_data *data)
+{
+	assert(data->drivertype == HIDPP20);
+
+	return data->hidpp20.quirk;
 }
 
 /* SteelSeries */
