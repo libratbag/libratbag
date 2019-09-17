132632f93af14dc4b37dd8baad8c6053883eac27
Filipe Laíns <lains@archlinux.org>
2019-09-17 09:50:05 +0100

    tests: update to ratbag-emu's new id format
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- test/test_emulation/test_steelseries.py
+++ test/test_emulation/test_steelseries.py
@@ -7,14 +7,14 @@ class TestSteelseriesDevice2(TestDevice):
     shortname = 'steelseries-rival310'
 
     def test_create(self, id, client):
-        subprocess.check_call(f"ratbagctl 'ratbag-emu test device #{id}' info >/dev/null", shell=True)
+        subprocess.check_call(f"ratbagctl 'ratbag-emu {id}' info >/dev/null", shell=True)
 
     def test_dpi(self, id, client, dpi_id=0):
         old_dpi = 500
         new_dpi = 1000
 
         client.set_dpi(id, dpi_id, old_dpi)
-        subprocess.check_call(f"ratbagctl 'ratbag-emu test device #{id}' resolution {str(dpi_id)} dpi set {str(new_dpi)}", shell=True)
+        subprocess.check_call(f"ratbagctl 'ratbag-emu {id}' resolution {str(dpi_id)} dpi set {str(new_dpi)}", shell=True)
 
         x = y = 5
         data = [
