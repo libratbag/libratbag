13d9026c6ad682a39f6a6bb42204c41d916936a3
Filipe Laíns <lains@archlinux.org>
2019-12-04 21:18:56 +0000

    ratbagd: make raw the default verbosity level
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- ratbagd/ratbagd.c
+++ ratbagd/ratbagd.c
@@ -617,12 +617,12 @@ int main(int argc, char *argv[])
 			return 0;
 		} else if (streq(argv[1], "--quiet")) {
 			log_level = LL_QUIET;
-		} else if (streq(argv[1], "--verbose=raw")) {
-			log_level = LL_RAW;
 		} else if (streq(argv[1], "--verbose")) {
+			log_level = LL_RAW;
+		} else if (streq(argv[1], "--verbose=debug")) {
 			log_level = LL_VERBOSE;
 		} else {
-			fprintf(stderr, "Usage: %s [--version | --quiet | --verbose[=raw]]\n",
+			fprintf(stderr, "Usage: %s [--version | --quiet | --verbose[=debug]]\n",
 				program_invocation_short_name);
 			r = -EINVAL;
 			goto exit;
