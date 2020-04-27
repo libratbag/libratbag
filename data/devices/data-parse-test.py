a7783e0724891a638336f11793be45ac5a3eebf8
Filipe Laíns <lains@archlinux.org>
2020-04-27 13:16:38 +0100

    hidpp20: allow setting a default report rate
    In case we can't get the report rate value from the device, allow
    setting in the device file.
    
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- data/devices/data-parse-test.py
+++ data/devices/data-parse-test.py
@@ -168,7 +168,7 @@ def check_section_hidpp10(section):
 
 
 def check_section_hidpp20(section):
-    permitted = ['DeviceIndex', 'Leds', 'Quirk']
+    permitted = ['DeviceIndex', 'Leds', 'ReportRate', 'Quirk']
     for key in section.keys():
         assertIn(key, permitted)
 
