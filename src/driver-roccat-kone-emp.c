f3052c5ff10f9794da7fbb29b4676ee6901f13f7
Yaroslav Chvanov <yaroslav.chvanov@gmail.com>
2023-02-25 12:51:00 +0300

    roccat-kone-emp: don't enable key-type button actions
    Not actually implemented.

--- src/driver-roccat-kone-emp.c
+++ src/driver-roccat-kone-emp.c
@@ -843,7 +843,6 @@ roccat_read_button(struct ratbag_button *button)
 //          __FILE__, __LINE__);
 
 	ratbag_button_enable_action_type(button, RATBAG_BUTTON_ACTION_TYPE_BUTTON);
-	ratbag_button_enable_action_type(button, RATBAG_BUTTON_ACTION_TYPE_KEY);
 	ratbag_button_enable_action_type(button, RATBAG_BUTTON_ACTION_TYPE_SPECIAL);
 	ratbag_button_enable_action_type(button, RATBAG_BUTTON_ACTION_TYPE_MACRO);
 
