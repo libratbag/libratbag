46223c1ea5c10ae76150c6ffb913301263140066
Sewer56 <sewer56lol@googlemail.com>
2025-03-11 19:04:19 +0000

    Fixed: LED write functionality for G502X PLUS
    Inspired by https://github.com/Nadeflore/libratbag/commit/94bd172a19024733b612d2cd65e3f3e5c3fca07e

--- src/hidpp20.c
+++ src/hidpp20.c
@@ -110,6 +110,7 @@ hidpp20_get_quirk_string(enum hidpp20_quirk quirk)
 	CASE_RETURN_STRING(HIDPP20_QUIRK_NONE);
 	CASE_RETURN_STRING(HIDPP20_QUIRK_G305);
 	CASE_RETURN_STRING(HIDPP20_QUIRK_G602);
+	CASE_RETURN_STRING(HIDPP20_QUIRK_G502X_PLUS);
 	}
 
 	abort();
@@ -1832,7 +1833,8 @@ union hidpp20_internal_profile {
 		} name;
 		struct hidpp20_internal_led leds[2]; /* G303, g502, g900 only */
 		struct hidpp20_internal_led alt_leds[2];
-		uint8_t free[2];
+		uint8_t custom_animation_index; // G502X Plus, G705
+		uint8_t free; // unused
 		uint16_t crc;
 	} __attribute__((packed)) profile;
 };
@@ -2960,6 +2962,18 @@ hidpp20_onboard_profiles_write_profile(struct hidpp20_device *device,
 	hidpp20_buttons_from_cpu(profile, pdata->profile.buttons, profiles_list->num_buttons);
 
 	memcpy(pdata->profile.name.txt, profile->name, sizeof(profile->name));
+
+	// Mice like G502X can store custom animations onboard.
+	// ratbag does not support this, so we set it to disable it to ensure
+	// that the actual lighting matches the user's settings.
+	if (device->quirk == HIDPP20_QUIRK_G502X_PLUS)
+	{
+		// Note(sewer56): There are two known mice which use this field;
+		// G502X PLUS and G705. [And I only own one of these.] But it's not known how 
+		// prevalent this is elsewhere, so in the interest of defensive programming;
+		// we're not setting this on unknown mice. 
+		pdata->profile.custom_animation_index = 0x00;
+	}
 
 	rc = hidpp20_onboard_profiles_write_sector(device, sector, sector_size, data, true);
 	if (rc < 0) {
